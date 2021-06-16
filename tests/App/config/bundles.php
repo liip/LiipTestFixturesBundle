b621f347a83b6edd2440c7e5ab925bc10423b347
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2021-06-16 23:00:32 +0200

    Remove monolog

--- tests/App/config/bundles.php
+++ tests/App/config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['all' => true],
