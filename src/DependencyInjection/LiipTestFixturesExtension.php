203df6491eb0d2981317030cca06ecd598b48eda
chris <chris@sky-scripts.de>
2024-06-10 13:12:28 +0200

    fix: deprecation for Extension, since symfony 7.1 (#313)
    * fix: deprecation for Extension, since symfony 7.1
    
    ---------
    
    Co-authored-by: Christopher Georg <christopher.georg@sr-travel.de>

--- src/DependencyInjection/LiipTestFixturesExtension.php
+++ src/DependencyInjection/LiipTestFixturesExtension.php
@@ -15,8 +15,8 @@ namespace Liip\TestFixturesBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class LiipTestFixturesExtension extends Extension
 {
