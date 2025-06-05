d747529323acbb2e0029faea9a80bb27e44e49b4
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2025-06-05 22:08:34 +0200

    chore: remove @inheritdoc for PHP-CS-Fixer

--- tests/AppConfigPgsqlCacheDb/AppConfigPgsqlKernelCacheDb.php
+++ tests/AppConfigPgsqlCacheDb/AppConfigPgsqlKernelCacheDb.php
@@ -19,9 +19,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class AppConfigPgsqlKernelCacheDb extends AppConfigPgsqlKernel
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getCacheDir(): string
     {
         return __DIR__.'/var/cache/';
